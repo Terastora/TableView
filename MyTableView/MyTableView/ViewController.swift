6dbf039a83eefd22e183e71c02225119bc3be0ac
星んでん <karladonis56@gmail.com>
2024-11-21 14:57:49 +0800

    Add files via upload

---
+++ MyTableView/MyTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTableView
+//
+//  Created by ADONIS, KARL ANTHONY on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
